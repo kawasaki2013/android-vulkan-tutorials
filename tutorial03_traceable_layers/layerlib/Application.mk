f39647450cec2ec17b12d41c7fcbc306077dbb07
guanghuafan <gfan@google.com>
2016-09-06 10:46:12 -0700

    adding cmake support

---
+++ tutorial03_traceable_layers/layerlib/Application.mk
@@ -0,0 +1,8 @@
+PP_ABI := armeabi-v7a arm64-v8a x86 x86_64 mips mips64
+APP_PLATFORM := android-24
+# Shaderc builds the libs into a directory with APP_STL being part of it
+# CMakeLists.ext would need to be updated if stl type is changed
+APP_STL := gnustl_static
+NDK_TOOLCHAIN_VERSION := clang
+APP_CFLAGS += -I/Users/gfan/dev/android-ndk-r13-beta2/sources/android/native_app_glue
+
